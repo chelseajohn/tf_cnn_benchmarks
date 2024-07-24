fa0c57cc21f3e693403161477b6f065ec7aa88c3
john2 <john2@jrlogin12.jureca>
2024-07-24 12:23:14 +0200

    fixed power meas

--- scripts/tf_cnn_benchmarks/get_power_rsmi.py
+++ scripts/tf_cnn_benchmarks/get_power_rsmi.py
@@ -13,10 +13,10 @@ except:
         rocm_path = "/opt/rocm/"
     sys.path.append(os.path.join(rocm_path, "rocm_smi/bindings"))
     from rsmiBindings import *
+
 from multiprocessing import Process, Queue, Event
 
 def power_loop(queue, event, interval):
-    rocmsmi = initRsmiBindings(silent=False)
     ret = rocmsmi.rsmi_init(0)
     if rsmi_status_t.RSMI_STATUS_SUCCESS != ret:
         raise RuntimeError("Failed initializing rocm_smi library")
@@ -30,6 +30,7 @@ def power_loop(queue, event, interval):
     }
     power_value_dict['timestamps'] = []
     last_timestamp = time.time()
+    dev_pwr_map = { id: True for id in device_list }
     start_energy_list = []
     for id in device_list:
         energy = c_uint64()
@@ -41,14 +42,19 @@ def power_loop(queue, event, interval):
                 byref(energy_timestamp))
         if rsmi_status_t.RSMI_STATUS_SUCCESS != ret:
             raise RuntimeError(f"Failed getting Power of device {id}")
+        if 0 == energy.value:
+            dev_pwr_map[id] = False
         start_energy_list.append(round(energy.value*energy_resolution.value,2)) # unit is uJ
 
     while not event.is_set():
         for id in device_list:
             power = c_uint32()
-            ret = rocmsmi.rsmi_dev_power_ave_get(id, 0, byref(power))
-            if rsmi_status_t.RSMI_STATUS_SUCCESS != ret:
-                raise RuntimeError(f"Failed getting Power of device {id}")
+            if not dev_pwr_map[id]:
+                power.value = 0
+            else:
+                ret = rocmsmi.rsmi_dev_power_ave_get(id, 0, byref(power))
+                if rsmi_status_t.RSMI_STATUS_SUCCESS != ret:
+                    raise RuntimeError(f"Failed getting power of device {id}: {ret}")
             power_value_dict[id].append(power.value*1e-6) # value is uW
         timestamp = time.time()
         power_value_dict['timestamps'].append(timestamp)
@@ -72,67 +78,8 @@ def power_loop(queue, event, interval):
     energy_list = [ (energy*1e-6)/3600 for energy in energy_list] # convert uJ to Wh
     queue.put(power_value_dict)
     queue.put(energy_list)
-    rocmsmi.rsmi_shut_down()
-
-class power(object):
-    def init(self, power_value_dict : dict[str,list[float]]):
-        self.rocmsmi = initRsmiBindings(silent=False)
-        ret = self.rocmsmi.rsmi_init(0)
-        if rsmi_status_t.RSMI_STATUS_SUCCESS != ret:
-            raise RuntimeError("Failed initializing rocm_smi library")
-        device_count = c_uint32(0)
-        ret = self.rocmsmi.rsmi_num_monitor_devices(byref(device_count))
-        if rsmi_status_t.RSMI_STATUS_SUCCESS != ret:
-            raise RuntimeError("Failed enumerating ROCm devices")
-        self.device_list = list(range(device_count.value))
-        power_value_dict.update({
-            f"rocm:{id}" : [] for id in self.device_list
-        })
-        self.start_energy_list = []
-        for id in self.device_list:
-            energy = c_uint64()
-            energy_timestamp = c_uint64()
-            energy_resolution = c_float()
-            ret = self.rocmsmi.rsmi_dev_energy_count_get(id, 
-                    byref(energy),
-                    byref(energy_resolution),
-                    byref(energy_timestamp))
-            if rsmi_status_t.RSMI_STATUS_SUCCESS != ret:
-                raise RuntimeError(f"Failed getting Power of device {id}")
-            self.start_energy_list.append(round(energy.value*energy_resolution.value,2)) # unit is uJ
-    def measure(self, power_value_dict : dict[str,list[float]]):
-        for id in self.device_list:
-            power = c_uint32()
-            ret = self.rocmsmi.rsmi_dev_power_ave_get(id, 0, byref(power))
-            if rsmi_status_t.RSMI_STATUS_SUCCESS != ret:
-                raise RuntimeError(f"Failed getting Power of device {id}")
-            power_value_dict[f"rocm:{id}"].append(float(power.value)*1e-6) # value is uW
-
-    def finalize(self, power_value_dict : dict[str,list[float]]):
-        energy_list = [0.0 for _ in self.device_list]
-        for id in self.device_list:
-            energy = c_uint64()
-            energy_timestamp = c_uint64()
-            energy_resolution = c_float()
-            ret = self.rocmsmi.rsmi_dev_energy_count_get(id, 
-                    byref(energy),
-                    byref(energy_resolution),
-                    byref(energy_timestamp))
-            if rsmi_status_t.RSMI_STATUS_SUCCESS != ret:
-                raise RuntimeError(f"Failed getting Power of device {id}")
-            energy_list[id] = round(energy.value*energy_resolution.value,2) - self.start_energy_list[id]
-
-        energy_list = [ (energy*1e-6)/3600 for energy in energy_list] # convert uJ to Wh
-        self.rocmsmi.rsmi_shut_down()
-
-        return {
-            "energy_from_counter": pd.DataFrame({
-                f"rocm:{idx}" : [value] for idx,value in zip(self.device_list,energy_list)
-            })
-        }
 
 class GetPower(object):
-
     def __enter__(self):
         self.end_event = Event()
         self.power_queue = Queue()
@@ -155,3 +102,21 @@ class GetPower(object):
         energy_df = self.df.loc[:,self.df.columns != 'timestamps'].astype(float).multiply(self.df["timestamps"].diff(),axis="index")/3600
         _energy = energy_df[1:].sum(axis=0).values.tolist()
         return _energy,self.energy_list_counter
+
+
+if __name__ == "__main__":
+    with GetPower() as measured_scope:
+        print('Measuring Energy during main() call')
+        try:
+            main(args)
+        except Exception as exc:
+            import traceback
+            print(f"Errors occured during training: {exc}")
+            print(f"Traceback: {traceback.format_exc()}")
+    print("Energy data:")
+    print  (measured_scope.df)
+    print("Energy-per-GPU-list:")
+    energy_int,energy_cnt = measured_scope.energy()
+    print(f"integrated: {energy_int}")
+    print(f"from counter: {energy_cnt}")
+    f.close()
